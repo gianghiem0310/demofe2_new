11be1dcb76e2d8ecbc54e16f55ffd1c4d6ad2a62
gianghiem0310 <jangiim93nt@gmail.com>
2023-05-23 19:05:50 +0700

    edit

--- Demo/public/js/app.js
+++ Demo/public/js/app.js
@@ -4,6 +4,8 @@ let cautraloi_2 = document.querySelector(".cautraloi_2");
 let box_dapan_1 = document.querySelector(".box_dapan_1");
 let box_dapan_2 = document.querySelector(".box_dapan_2");
 let box_hoathinh = document.querySelector(".box_hoathinh");
+let box_phaohoa = document.querySelector(".box_phaohoa");
+let star = document.querySelectorAll(".star");
 function ranDom() {
     return Math.floor(Math.random() * 50);
 }
@@ -21,9 +23,13 @@ let dapanDung;
 let dapanSai;
 let viTriDung;
 function game() {
+    changeColorOfStarComeBack();
+    box_phaohoa.style.width = 0+"px";
+    box_phaohoa.style.height = 0+"px";
     box_hoathinh.style.width = 0+"px";
     box_hoathinh.style.height = 0+"px";
-   box_hoathinh.innerHTML="";
+    box_hoathinh.innerHTML="";
+
     so1 = ranDom();
     so2 = ranDom();
     phepTinh = ranDomPhepTinh();
@@ -53,6 +59,9 @@ game();
 box_dapan_1.addEventListener('click', function () {
     if (box_dapan_1.textContent == dapanDung) {
         box_hoathinh.innerHTML+=`<img class="img-fluid hoat_hoa" src="./public/images/chienthang.gif">`;
+        changeColorOfStar();
+        box_phaohoa.style.width = 100+"%";
+        box_phaohoa.style.height = 450+"px";
         box_hoathinh.style.width = 400+"px";
         box_hoathinh.style.height = 400+"px";
         setTimeout(() => {
@@ -71,6 +80,9 @@ box_dapan_1.addEventListener('click', function () {
 box_dapan_2.addEventListener('click', function () {
     if (box_dapan_2.textContent == dapanDung) {
         box_hoathinh.innerHTML+=`<img class="img-fluid hoat_hoa" src="./public/images/chienthang.gif">`;
+        changeColorOfStar();
+        box_phaohoa.style.width = 100+"%";
+        box_phaohoa.style.height = 450+"px";
         box_hoathinh.style.width = 400+"px";
         box_hoathinh.style.height = 400+"px";
         setTimeout(() => {
@@ -86,6 +98,19 @@ box_dapan_2.addEventListener('click', function () {
         }, 1000); 
     }
 })
+
+function changeColorOfStarComeBack() {
+    star.forEach(element => {
+        element.style.background = "#d3a145";
+        element.style.fill = "#000";
+    });
+}
+function changeColorOfStar() {
+    star.forEach(element => {
+        element.style.background = "orange";
+        element.style.fill = "yellow";
+    });
+}
 
 
 
